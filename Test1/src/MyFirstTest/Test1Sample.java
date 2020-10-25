af14138c8c2bb07259ffb208bbeaf66900ce96c3
nagarjuna4241 <knagarjunareddy7@gmail.com>
2020-10-25 11:17:10 +0530

    First Commit

---
+++ Test1/src/MyFirstTest/Test1Sample.java
@@ -0,0 +1,12 @@
+package MyFirstTest;
+
+public class Test1Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World.....!");
+		System.out.println(1+2 +"\'addition"+  2+3);
+
+	}
+
+}
